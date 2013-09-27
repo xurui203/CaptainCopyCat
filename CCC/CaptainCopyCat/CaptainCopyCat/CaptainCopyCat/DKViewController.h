cb4f33fba1f8179f0661f794907bb046276392d8
yan2 <niou@wl-10-190-48-90.wireless.duke.edu>
2013-09-27 16:29:07 -0400

    folder

---
+++ CCC/CaptainCopyCat/CaptainCopyCat/CaptainCopyCat/DKViewController.h
@@ -0,0 +1,13 @@
+//
+//  DKViewController.h
+//  CaptainCopyCat
+//
+//  Created by Xu Rui on 27/9/13.
+//  Copyright (c) 2013 Xu Rui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DKViewController : UIViewController
+
+@end
---
+++ CCC/CaptainCopyCat/CaptainCopyCat/CaptainCopyCat/DKViewController.m
@@ -0,0 +1,29 @@
+//
+//  DKViewController.m
+//  CaptainCopyCat
+//
+//  Created by Xu Rui on 27/9/13.
+//  Copyright (c) 2013 Xu Rui. All rights reserved.
+//
+
+#import "DKViewController.h"
+
+@interface DKViewController ()
+
+@end
+
+@implementation DKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
