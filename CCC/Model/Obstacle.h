3b2b4350326a00cfa1553c3b0948ea2144e2fabc
xurui203 <xurui203@gmail.com>
2013-10-06 03:28:36 -0400

    Added instructions page, need to do maze

---
+++ CCC/Model/Obstacle.h
@@ -0,0 +1,13 @@
+//
+//  Obstacle.h
+//  CCC
+//
+//  Created by Xu Rui on 6/10/13.
+//  Copyright (c) 2013 Ann Niou. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface Obstacle : SKPhysicsContact
+
+@end
---
+++ CCC/Model/Obstacle.m
@@ -0,0 +1,13 @@
+//
+//  Obstacle.m
+//  CCC
+//
+//  Created by Xu Rui on 6/10/13.
+//  Copyright (c) 2013 Ann Niou. All rights reserved.
+//
+
+#import "Obstacle.h"
+
+@implementation Obstacle
+
+@end
