ccbb2eca5217a19edb0d612e752ebcfd4f4fe415
xurui203 <xurui203@gmail.com>
2013-10-06 01:59:34 -0400

    Removed unused classes, connected view controllers in storyboard

---
+++ CCC/KangerooModuleViewController.h
@@ -0,0 +1,13 @@
+//
+//  KangerooViewController.h
+//  CCC
+//
+//  Created by Xu Rui on 6/10/13.
+//  Copyright (c) 2013 Ann Niou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KangerooViewController : UIViewController
+
+@end
