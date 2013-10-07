77a5de29e9208683079fc836b2db1d0550e70f42
yan2 <niou@TTS-120271ML.local>
2013-10-07 04:08:54 -0400

    created popover class

---
+++ CCC/SuperpowerDrawer.h
@@ -0,0 +1,13 @@
+//
+//  SuperpowerDrawer.h
+//  CCC
+//
+//  Created by Ann Niou on 10/7/13.
+//  Copyright (c) 2013 Ann Niou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuperpowerDrawer : UIPopoverController
+
+@end
---
+++ CCC/SuperpowerDrawer.m
@@ -0,0 +1,13 @@
+//
+//  SuperpowerDrawer.m
+//  CCC
+//
+//  Created by Ann Niou on 10/7/13.
+//  Copyright (c) 2013 Ann Niou. All rights reserved.
+//
+
+#import "SuperpowerDrawer.h"
+
+@implementation SuperpowerDrawer
+
+@end
