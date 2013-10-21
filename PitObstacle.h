656bf5b0e09e13022de596bd4424d65555935dcc
Rakannan <guestuser@wl-10-190-208-191.wireless.duke.edu>
2013-10-21 01:23:25 -0400

    PitObstacle Class
    Lez gooo

---
+++ PitObstacle.h
@@ -0,0 +1,13 @@
+//
+//  PitObstacle.h
+//  CCC
+//
+//  Created by guest user on 10/21/13.
+//  Copyright (c) 2013 Ann Niou. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface PitObstacle : SKScene
+
+@end
---
+++ PitObstacle.m
@@ -0,0 +1,13 @@
+//
+//  PitObstacle.m
+//  CCC
+//
+//  Created by guest user on 10/21/13.
+//  Copyright (c) 2013 Ann Niou. All rights reserved.
+//
+
+#import "PitObstacle.h"
+
+@implementation PitObstacle
+
+@end
